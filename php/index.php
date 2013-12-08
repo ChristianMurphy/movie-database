474245a02f3e66498df80bb313e8364941b9e868
Christian Murphy <christian.murphy@asu.edu>
2013-12-08 14:40:38 -0800

    creating php search for database

--- php/index.php
+++ php/index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Movie Database</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
@@ -19,8 +17,45 @@
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js" defer></script>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar navbar-default" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Movie Database</a>
+      </div>
 
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </nav>
+
+    <main class="container">
+      <h1>Movie Search</h1>
+
+      <form role="form">
+        <div class="form-group">
+          <label for="query-type">Type</label>
+          <select class="form-control" id="query-type">
+            <option>Person</option>
+            <option>Movie</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="attribute-value">Value</label>
+          <input type="password" class="form-control" id="attribute-value" placeholder="name">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </main>
    
   </body>
 </html>
